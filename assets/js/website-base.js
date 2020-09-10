741d223fab80f12af13fcce3c96f474be2b3525a
Hans5958 <email5958.1@gmail.com>
2020-09-10 23:13:01 +0700

    Make script run async

--- assets/js/website-base.js
+++ assets/js/website-base.js
@@ -5,30 +5,30 @@ const update = () => {
 	document.body.classList.toggle("dark")
 	darkTheme = !darkTheme
 	localStorage.setItem("darkTheme", darkTheme)
-	try {
-		if (darkTheme) {
-			toggle.innerHTML = '<span class="iconify" data-icon="fa-solid:sun" data-inline="false"></span>'
-		} else {
-			toggle.innerHTML = '<span class="iconify" data-icon="fa-solid:moon" data-inline="false"></span>'
-		}
-	} finally {}
+	if (darkTheme) {
+		toggle.innerHTML = '<span class="iconify" data-icon="fa-solid:sun" data-inline="false"></span>'
+	} else {
+		toggle.innerHTML = '<span class="iconify" data-icon="fa-solid:moon" data-inline="false"></span>'
+	}
 }
 
-document.addEventListener("DOMContentLoaded", () => {
-	toggle = document.querySelector("#dark-toggle")
+if (darkTheme) {
+	toggle.innerHTML = '<span class="iconify" data-icon="fa-solid:sun" data-inline="false"></span>'
+} else {
+	toggle.innerHTML = '<span class="iconify" data-icon="fa-solid:moon" data-inline="false"></span>'
+}
 
-	if (localStorage.getItem("darkTheme")) {
-		if (localStorage.getItem("darkTheme") == "true") {
-			update()
-		}
-	} else {
-		if (window.matchMedia("(prefers-color-scheme: dark)")) {
-			update()
-		}
-	}	
-	
-	toggle.addEventListener("click", () => {
+if (localStorage.getItem("darkTheme")) {
+	if (localStorage.getItem("darkTheme") == "true") {
 		update()
-	})
+	}
+} else {
+	if (window.matchMedia("(prefers-color-scheme: dark)")) {
+		update()
+	}
+}	
+
+toggle.addEventListener("click", () => {
+	update()
 })
 
